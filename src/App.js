c31b8cfcb2cdc55ae036869ad2ff92a1bb9e6e02
Zar Li Hnin <sakurazarli@gmail.com>
2020-08-10 16:58:31 +0630

    testing-sample-calculator

--- src/App.js
+++ src/App.js
@@ -1,26 +1,57 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {createRef,useState} from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+
+
+class App extends React.Component {
+      
+      constructor(props){
+        super(props)
+        this.state = { firstNum: '', secondNum: '', result: '' }
+      }
+      
+      add(){
+        console.log("First Number: ", this.state.firstNum, " - SecondNumber: ", this.state.secondNum)
+        this.setState({ result: parseInt(this.state.firstNum) + parseInt(this.state.secondNum) })
+        console.log(this.state)
+      }
+      subtract(){
+        console.log("First Number: ", this.state.firstNum, " - SecondNumber: ", this.state.secondNum)
+        this.setState({ result: parseInt(this.state.firstNum) - parseInt(this.state.secondNum) })
+        console.log(this.state)
+      }
+      multiply(){
+        console.log("First Number: ", this.state.firstNum, " - SecondNumber: ", this.state.secondNum)
+        this.setState({ result: parseInt(this.state.firstNum) * parseInt(this.state.secondNum) })
+        console.log(this.state)
+      }
+      divide(){
+        console.log("First Number: ", this.state.firstNum, " - SecondNumber: ", this.state.secondNum)
+        this.setState({ result: parseInt(this.state.firstNum) / parseInt(this.state.secondNum) })
+        console.log(this.state)
+      }
+      render() {
+        return(
+          <div>
+            <h1>Add Two Numbers</h1>
+     
+            <input type="text" className="inputStyle" value={this.state.firstNum} onChange={ (eve) => { this.setState({ firstNum: eve.target.value }) } }/>
+            <br/><br/>
+     
+            <input type="text" className="inputStyle" value={this.state.secondNum} onChange={ (eve) => { this.setState({ secondNum: eve.target.value })} } />
+            <br/><br/>
+     
+            <button onClick={()=>{this.add()}} >+</button>
+            <button onClick={()=>{this.subtract()}} >-</button>
+            <button onClick={()=>{this.multiply()}} >*</button>
+            <button onClick={()=>{this.divide()}} >/</button>
+            <br/><br/>
+     
+            <input type="text" value={this.state.result} />
+     
+            <br/><br/>
+          </div>
+        )
+      }
+      }
 
 export default App;
